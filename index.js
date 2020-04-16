98c7e15e8d249d25d9201d17dda34c2d6d7173fe
digichu <digichu@gmail.com>
2020-04-16 09:33:42 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var status = "ok"
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', msg + status);
   });
 });
 
