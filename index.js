a66ed172327cc2b380caf246d48faa740405990f
digichu <digichu@gmail.com>
2020-04-18 18:55:03 -0700

    yo

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-var status = "ok"
+var status = "ok";
+
+var test = new Array();
+test.push({var1:1, var2: new Array("ok", "test)")})
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -18,3 +21,7 @@ io.on('connection', function(socket){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+function test1(){
+  io.emit('something', test);
+}
